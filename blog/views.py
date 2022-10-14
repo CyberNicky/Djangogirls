b8be06a2867a47067d8a9f86e40748c362419f08
Monique <nickyalbuquerque2008@gmail.com>
2022-10-14 12:57:16 -0300

    fix: view posts

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    print(posts)
     return render(request, 'blog/post_list.html', {'posts': posts})
    
 def post_detail(request, pk):
